869dcb9336d02d8f2290654f5786cad3b977a658
Arthur Le Floch <arthur.le_floch@bordeaux-inp.fr>
2024-12-01 21:19:57 +0100

    Add solution

---
+++ day1/main.py
@@ -0,0 +1,31 @@
+# Day 1: Historian Hysteria
+
+# Parsing
+first_list, second_list = [], []
+
+with open('input.txt') as f:
+	input_lines = f.readlines()
+	
+	for line in input_lines:
+		numbers = line.split("   ")
+		first_list.append(int(numbers[0]))
+		second_list.append(int(numbers[1]))
+
+## Part 1 : distance
+first_list.sort()
+second_list.sort()
+
+distance = sum(abs(first - second) for first, second in zip(first_list, second_list))
+print("Distance between lists :", distance)
+
+## Part 2 : similarity
+first_set = set(first_list)
+occurrences = {}
+
+for second in second_list:
+	if second in first_set:
+		occurrences[second] = occurrences.get(second, 0) + 1
+
+similarity = sum(first * occurrences.get(first, 0) for first in first_list)
+
+print("Similarity between lists :", similarity)
