2f21d9923600a1f88d4efdd8538f2898a7e0e43c
Arthur Le Floch <arthur.le_floch@bordeaux-inp.fr>
2024-12-01 21:41:04 +0100

    Format code

--- day1/main.py
+++ day1/main.py
@@ -5,22 +5,19 @@ from collections import Counter
 first_list, second_list = [], []
 
 with open('input.txt') as f:
-	input_lines = f.readlines()
-	
-	for line in input_lines:
-		numbers = line.split()
-		first_list.append(int(numbers[0]))
-		second_list.append(int(numbers[1]))
+    for line in f.readlines():
+        numbers = line.split()
+        first_list.append(int(numbers[0]))
+        second_list.append(int(numbers[1]))
 
-## Part 1 : distance
+# Part 1 : distance
 first_list.sort()
 second_list.sort()
 
-distance = sum(abs(first - second) for first, second in zip(first_list, second_list))
+distance = sum(abs(a - b) for a, b in zip(first_list, second_list))
 print("Distance between lists :", distance)
 
-## Part 2 : similarity
+# Part 2 : similarity
 occurrences = Counter(second_list)
-similarity = sum(first * occurrences[first] for first in first_list)
-
+similarity = sum(x * occurrences[x] for x in first_list)
 print("Similarity between lists :", similarity)
