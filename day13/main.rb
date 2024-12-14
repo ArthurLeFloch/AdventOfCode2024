9b5dadcc25b713b4d9ba3a340e2fa7954c2df7a7
Arthur Le Floch <arthur.le_floch@bordeaux-inp.fr>
2024-12-14 09:56:40 +0100

    Use , ,  to get latest matches, simplify equation class

--- day13/main.rb
+++ day13/main.rb
@@ -1,75 +1,58 @@
 # Day 13: Claw Contraption
 
-class Equation
-  attr_accessor :a_dx, :a_dy, :b_dx, :b_dy, :p_x, :p_y
-
-  def initialize(a_dx, a_dy, b_dx, b_dy, p_x, p_y)
-    @a_dx = a_dx
-    @a_dy = a_dy
-    @b_dx = b_dx
-    @b_dy = b_dy
-    @p_x = p_x
-    @p_y = p_y
+class LineIntersection
+  def initialize(line_a, line_b, point)
+    @a_dx, @a_dy = line_a
+    @b_dx, @b_dy = line_b
+    @p_x, @p_y = point
   end
 
   def solve()
     det = @a_dx * @b_dy - @a_dy * @b_dx
-    if det == 0
-      return nil
-    end
+    return nil if det.zero?
+
     x = (@b_dy * @p_x - @b_dx * @p_y) / det.to_f
     y = (-@a_dy * @p_x + @a_dx * @p_y) / det.to_f
+    return nil if x % 1 != 0 || y % 1 != 0
 
-    if (x % 1 != 0 || y % 1 != 0)
-      return nil
-    end
-
-    return [x.to_i, y.to_i]
+    [x.to_i, y.to_i]
   end
 end
 
 class Problem
-  attr_accessor :equations
+  BUTTON_PATTERN = /Button (A|B): X\+(\d+), Y\+(\d+)/
+  PRIZE_PATTERN = /Prize: X=(\d+), Y=(\d+)/
 
-  def initialize(file_path, offset = 0)
+  attr_accessor :intersections
+
+  def initialize(file_path, prize_offset = 0)
     lines = File.read(file_path).split("\n")
 
-    @equations = []
+    @intersections = []
 
-    button_matcher = /Button (A|B): X\+(\d+), Y\+(\d+)/
-    prize_matcher = /Prize: X=(\d+), Y=(\d+)/
-
-    last_a = nil
-    last_b = nil
+    button_a = button_b = nil
 
     lines.each do |line|
-      match_button = line.match(button_matcher)
-      match_prize = line.match(prize_matcher)
-
-      if match_button
-        x = match_button[2].to_i
-        y = match_button[3].to_i
-        if match_button[1] == "A"
-          last_a = [x, y]
-        else
-          last_b = [x, y]
+      case line
+      when BUTTON_PATTERN
+        button = [$2.to_i, $3.to_i]
+        $1 == "A" ? button_a = button : button_b = button
+      when PRIZE_PATTERN
+        prize = [$1.to_i + prize_offset, $2.to_i + prize_offset]
+        if button_a && button_b
+          intersections << LineIntersection.new(button_a, button_b, prize)
         end
-      elsif match_prize
-        x = match_prize[1].to_i + offset
-        y = match_prize[2].to_i + offset
-        @equations.push(Equation.new(last_a[0], last_a[1], last_b[0], last_b[1], x, y))
       else
-        last_a = nil
-        last_b = nil
+        button_a = button_b = nil
       end
     end
   end
 end
 
-def first_part(problem)
+def solver(problem)
   sum = 0
-  problem.equations.each do |equation|
-    solution = equation.solve()
+  problem.intersections.each do |intersection|
+    solution = intersection.solve()
     if solution != nil
       sum += 3 * solution[0] + solution[1]
     end
@@ -77,8 +60,8 @@ def first_part(problem)
   return sum
 end
 
-problem = Problem.new("input.txt")
-puts "First part: #{first_part(problem)}"
+part1_input = Problem.new("input.txt")
+puts "First part: #{solver(part1_input)}"
 
-second_problem = Problem.new("input.txt", 10000000000000)
-puts "Second part: #{first_part(second_problem)}"
+part2_input = Problem.new("input.txt", 10_000_000_000_000)
+puts "Second part: #{solver(part2_input)}"
