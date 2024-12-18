a40fa4ed428ed13e3b5bbb078175c66cefc9f82a
Arthur Le Floch <arthur.le_floch@bordeaux-inp.fr>
2024-12-18 16:58:21 +0100

    Clean code

--- day18-Julia/main.jl
+++ day18-Julia/main.jl
@@ -6,16 +6,13 @@ struct Point
 end
 
 function parse_input(file_path)
-    f = open(file_path, "r")
     points = Vector{Point}()
-    while !eof(f)
-        s = readline(f)
-        values = split(s, ",")
-        x = parse(Int, values[1]) # Julia's vectors are 1-indexed
-        y = parse(Int, values[2])
-        push!(points, Point(x, y))
+    open(file_path, "r") do f
+        for line in eachline(f)
+            x, y = parse.(Int, split(line, ","))
+            push!(points, Point(x + 1, y + 1)) # Julia is 1-indexed, offset needed for arrays later
+        end
     end
-    close(f)
     return points
 end
 
@@ -29,17 +26,16 @@ function debug_map(map, map_size)
 end
 
 function is_valid(point, map_size)
-    return point.x >= 0 && point.x < map_size && point.y >= 0 && point.y < map_size
+    return point.x >= 1 && point.x <= map_size && point.y >= 1 && point.y <= map_size
 end
 
 function dijkstra(map, map_size)
-    from = Point(0, 0)
-    dest = Point(map_size - 1, map_size - 1)
+    from = Point(1, 1)
+    dest = Point(map_size, map_size)
 
-    # Dijkstra's algorithm
     visited = falses(map_size, map_size)
-    distances = ones(map_size, map_size) * Inf
-    distances[from.y+1, from.x+1] = 0
+    distances = fill(Inf, map_size, map_size)
+    distances[from.y, from.x] = 0
 
     prev = Dict{Point,Point}()
 
@@ -47,7 +43,7 @@ function dijkstra(map, map_size)
 
     while !isempty(stack)
         pos = popfirst!(stack)
-        visited[pos.y+1, pos.x+1] = true
+        visited[pos.y, pos.x] = true
 
         if pos == dest
             break
@@ -55,17 +51,17 @@ function dijkstra(map, map_size)
 
         neighbors = [Point(pos.x + 1, pos.y), Point(pos.x - 1, pos.y),
             Point(pos.x, pos.y + 1), Point(pos.x, pos.y - 1)]
-        next_dist = distances[pos.y+1, pos.x+1]
+        next_dist = distances[pos.y, pos.x]
         for nb in neighbors
-            if !is_valid(nb, map_size) || visited[nb.y+1, nb.x+1] || map[nb.y+1, nb.x+1]
+            if !is_valid(nb, map_size) || map[nb.y, nb.x]
                 continue
             end
 
-            if next_dist >= distances[nb.y+1, nb.x+1]
+            if visited[nb.y, nb.x] || next_dist >= distances[nb.y, nb.x]
                 continue
             end
 
-            distances[nb.y+1, nb.x+1] = next_dist
+            distances[nb.y, nb.x] = next_dist
             prev[nb] = pos
             push!(stack, nb)
         end
@@ -86,33 +82,33 @@ end
 function first_part(points, map_size, maxi)
     map = falses(map_size, map_size)
     for i in 1:min(size(points, 1), maxi)
-        map[points[i].y+1, points[i].x+1] = true
+        map[points[i].y, points[i].x] = true
     end
-    
 
-	path = dijkstra(map, map_size)
+	debug_map(map, map_size)
+
+    path = dijkstra(map, map_size)
     return size(path, 1)
 end
 
 function second_part(points, map_size)
-	map = falses(map_size, map_size)
-	
-	for i in eachindex(points)
-		map[points[i].y+1, points[i].x+1] = true
-		path = dijkstra(map, map_size)
+    map = falses(map_size, map_size)
 
-		if path == []
-			return points[i]
-		end
-	end
+    for p in points
+        map[p.y, p.x] = true
+        if dijkstra(map, map_size) == []
+            return p
+        end
+    end
 end
 
 
-file_path = "input.txt"
-map_size = 71
-maxi = 1024
+const FILE_PATH = "input.txt"
+const MAP_SIZE = 71
+const MAXI = 1024
 
-points = parse_input(file_path)
+points = parse_input(FILE_PATH)
 
-println("First part: ", first_part(points, map_size, maxi))
-println("Second part: ", second_part(points, map_size))
+println("First part: $(first_part(points, MAP_SIZE, MAXI))")
+point = second_part(points, MAP_SIZE)
+println("Second part: ($(point.x - 1), $(point.y - 1))")
